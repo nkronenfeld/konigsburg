f654aeb35d5aa6198569959aeeb62bfad34261f0
Chris Bethune <cbethune@uncharted.software>
2018-07-11 00:52:32 -0400

    minor es6 cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const webpack = require('webpack');
 module.exports = {
-  context: path.resolve('.', 'web/js'),
   entry: {
-    app: './main.js',
+    app: './web/js/main.js',
   },
   output: {
-    path: path.resolve('.', 'dist'),
-    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'web/dist'),
+    filename: 'bundle.js',
   },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
 };
